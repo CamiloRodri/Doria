c71eeb23a98dcbead50b18c86d0e538225bef9cd
Sebastian Roldan <sebasoldanm@gmail.com>
2019-11-14 22:46:18 -0500

    Debugbar
    Publicar 
    php artisan vendor:publish --provider="Barryvdh\Debugbar\ServiceProvider"
    
    SI no aparece
    php artisan cache:clear
    php artisan view:clear
    php artisan config:clear
    php artisan debugbar:clear
    
    Abrir incógnito

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         Zizaco\Entrust\EntrustServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -228,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
